d4b9fb538a5ba2b67823211696ce73824d9ec9c4
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-10-02 13:37:05 +0100

    update

---
+++ MVCtest/MVCtest/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿        
+using System.ComponentModel.DataAnnotations;
+using Microsoft.VisualBasic;
+
+namespace MVCtest.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength (100)]
+        public string Name { get; set; }
+
+        public string DisplayOrder { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+    }
+}
