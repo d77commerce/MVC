e216e0ad3ee43a4118948aa2de5b8001247a7d19
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-10-03 13:58:51 +0100

    Add DB MSSQL

--- MVCtest/MVCtest/Controllers/CategoryController.cs
+++ MVCtest/MVCtest/Controllers/CategoryController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCtest.Data;
+using MVCtest.Models;
 
 namespace MVCtest.Controllers
 {
     public class CategoryController : Controller
     {
+            private  readonly  ApplicationDbContext dbCategoryContext;
+
+            public CategoryController(ApplicationDbContext _dbCategoryContext)
+            {
+                dbCategoryContext = _dbCategoryContext;
+            }
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> category  =dbCategoryContext.Categories;
+            return View(category);
         }
     }
 }
