642fb68e2d1c1d959da8abce9f6cdc6afb95a3fa
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-10-05 13:45:42 +0100

    New App - Web Forum

---
+++ ForumApp/ForumApp/Data/DataConstants.cs
@@ -0,0 +1,24 @@
+﻿namespace ForumApp.Data
+{
+    /// <summary>
+    /// Data layer constants
+    /// </summary>
+    public static class DataConstants
+    {
+        /// <summary>
+        /// Post Constants
+        /// </summary>
+        public static class Post
+        {
+            /// <summary>
+            /// Max Title length
+            /// </summary>
+            public const int TitleMaxPost = 50;
+            /// <summary>
+            /// Max Content length
+            /// </summary>
+        public const int ContentMaxPost = 1500;
+        }
+        
+    }
+}
---
+++ ForumApp/ForumApp/Data/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+using static  ForumApp.Data.DataConstants.Post;
+
+
+namespace ForumApp.Data.Models
+{
+    [Comment("Published posts")]
+    public class Post
+    {
+        [Comment("Post Identifier")]
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Comment("Title")]
+        [MaxLength(TitleMaxPost)]
+        public string Title { get; set; }
+        [Required]
+        [Comment("Content")]
+        [MaxLength(ContentMaxPost)]
+        public string Content { get; set; }
+    }
+}
