3a749b550a13d0bbc5c495815fce355117c1db11
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-10-07 00:28:02 +0100

    update

--- MVCtest/MVCtest/Controllers/CategoryController.cs
+++ MVCtest/MVCtest/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 using MVCtest.Data;
 using MVCtest.Models;
 
@@ -6,16 +7,42 @@ namespace MVCtest.Controllers
 {
     public class CategoryController : Controller
     {
-            private  readonly  ApplicationDbContext dbCategoryContext;
+        private readonly ApplicationDbContext dbCategoryContext;
 
-            public CategoryController(ApplicationDbContext _dbCategoryContext)
-            {
-                dbCategoryContext = _dbCategoryContext;
-            }
+        public CategoryController(ApplicationDbContext _dbCategoryContext)
+        {
+            dbCategoryContext = _dbCategoryContext;
+        }
+        /// <summary>
+        /// List of Category
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
-            IEnumerable<Category> category  =dbCategoryContext.Categories;
+
+            IEnumerable<Category> category = dbCategoryContext.Categories;
             return View(category);
+        }
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+        /// <summary>
+        /// Create New Category
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category newObject)
+        {
+            dbCategoryContext.Categories.Add(newObject);
+            dbCategoryContext.SaveChanges();
+            return new ContentResult()
+            {
+                Content = "<script language='javascript' type='text/javascript'>alert('Thanks for Feedback!');</script>"
+            };
+
         }
     }
 }
