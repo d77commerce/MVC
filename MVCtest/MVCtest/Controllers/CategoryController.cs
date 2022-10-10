4e2d8cac4a95b1875527fb173dd39814533085a4
Danail Tsachev <73217857+d77commerce@users.noreply.github.com>
2022-10-10 09:37:45 +0100

    db post

--- MVCtest/MVCtest/Controllers/CategoryController.cs
+++ MVCtest/MVCtest/Controllers/CategoryController.cs
@@ -13,6 +13,7 @@ namespace MVCtest.Controllers
         {
             dbCategoryContext = _dbCategoryContext;
         }
+
         /// <summary>
         /// List of Category
         /// </summary>
@@ -23,11 +24,13 @@ namespace MVCtest.Controllers
             IEnumerable<Category> category = dbCategoryContext.Categories;
             return View(category);
         }
+
         public IActionResult Create()
         {
             return View();
 
         }
+
         /// <summary>
         /// Create New Category
         /// </summary>
@@ -36,6 +39,11 @@ namespace MVCtest.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category newObject)
         {
+            if (newObject.Name == newObject.DisplayOrder)
+            {
+                ModelState.AddModelError("Name", "Invalid Name");
+            }
+
             if (ModelState.IsValid)
             {
                 dbCategoryContext.Categories.Add(newObject);
@@ -46,10 +54,43 @@ namespace MVCtest.Controllers
             return View(newObject);
 
         }
-        public IActionResult Edit()
+
+        public IActionResult Edit(int? id)
         {
+            if (id == null)
+            {
+                id = 2;
+                return View(id);
+            }
+
+            var categoryId = dbCategoryContext.Categories.Find(id);
+            var categoryFirst = dbCategoryContext.Categories.FirstOrDefault(u => u.Id == id);
+            var categorySingle = dbCategoryContext.Categories.SingleOrDefault(u => u.Id == id);
             return View();
 
+        }
+
+        /// <summary>
+        /// Create New Category
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category newObject)
+        {
+            if (newObject.Name == newObject.DisplayOrder)
+            {
+                ModelState.AddModelError("Name", "Invalid Name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                dbCategoryContext.Categories.Add(newObject);
+                dbCategoryContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(newObject);
         }
     }
 }
