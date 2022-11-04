5088a27de7980493d5e3717f1551a2a7ad4a35d6
d77co <d77co@DESKTOP-5299F04>
2022-11-04 01:35:14 +0000

    DB init

--- HouseRentingSystem/HouseRentingSystem/Program.cs
+++ HouseRentingSystem/HouseRentingSystem/Program.cs
@@ -1,10 +1,10 @@
-using HouseRentingSystem.Data;
+using HouseRentingSystem.Infrastructure.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
@@ -16,10 +16,10 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
 }
 else
 {
