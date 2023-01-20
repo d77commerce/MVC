e5a7d5cab0926d1ba8bce8c3f0a46e52757e49d4
d77commerce <d77commerce@hotmail.com>
2023-01-20 00:44:38 +0000

    add Summary

--- MVCtest/MVCtest/Controllers/CartController.cs
+++ MVCtest/MVCtest/Controllers/CartController.cs
@@ -19,7 +19,7 @@ namespace MVCtest.Controllers
 			_unitOfWork = unitOfWork;
 
 		}
-		public async Task<IActionResult> Index(int count)
+		public async Task<IActionResult> Index()
 		{
 			var claimsIdentity = (ClaimsIdentity)User.Identity;
 			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
@@ -40,7 +40,28 @@ namespace MVCtest.Controllers
 			shoppingCart.TotalSum = sum;
 			return View(shoppingCart);
 		}
-
+		public async Task<IActionResult> Summary()
+		{
+			/*var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+			double sum = 0;
+			ShoppingCartVM shoppingCart = new ShoppingCartVM()
+			{
+				ShoppingCarts = _unitOfWork.CartDb.GetAll(includeProperties: "Product,ApplicationUser")
+					.Where(u => u.ApplicationUserId.ToString() == claim.Value)
+			};
+			foreach (var cart in shoppingCart.ShoppingCarts)
+			{
+				cart.TotalPrice = ShoppingCartService.TotalPricePerQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
+					cart.Product.Price100);
+				cart.Price = ShoppingCartService.PricePerQuantity(cart.Count, cart.Product.Price, cart.Product.Price50,
+					cart.Product.Price100);
+				sum += cart.TotalPrice;
+			}
+			shoppingCart.TotalSum = sum;
+			return View(shoppingCart);*/
+			return View();
+		}
 		public async Task<IActionResult> PlusCountTask(int cartId)
 		{
 			var cart = _unitOfWork.CartDb.GetFirstOrDefault(c => c.Id == cartId);
